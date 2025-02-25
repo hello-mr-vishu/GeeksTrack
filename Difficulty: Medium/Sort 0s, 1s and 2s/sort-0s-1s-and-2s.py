58cdc5425a0edaa2002566e517c980ecb1eb0fc0
Vishnu Vardhan <130839613+hello-mr-vishu@users.noreply.github.com>
2025-02-25 09:02:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.py
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.py
@@ -5,9 +5,24 @@
 class Solution:
     # Function to sort an array of 0s, 1s, and 2s
     def sort012(self, arr):
-        arr.sort()
+        cnt0 =0
+        cnt1 =0
+        cnt2 =0
+        for i in range(len(arr)):
+            if arr[i]==0:
+                cnt0+=1
+            elif arr[i]==1:
+                cnt1+=1
+            else:
+                cnt2+=1
+            
+        for j in range(cnt0):
+            arr[j] = 0
+        for j in range(cnt0,cnt1+cnt0):
+            arr[j] = 1
+        for j in range(cnt1+cnt0,cnt0+cnt1+cnt2):
+            arr[j] = 2
         return arr
-
 #{ 
  # Driver Code Starts.
 def main():
